cf79809a885f0225fe36c9e116139425dcad85fc
S7K <sala7khaled.s7k@gmail.com>
2020-06-05 05:56:25 +0200

    first commit

---
+++ Calculator/Controller/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Salah Khaled on 4/25/20.
+//  Copyright © 2020 Salah Khaled. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var isFinishedTypingNumber: Bool = true
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Can not convert display value into double")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        isFinishedTypingNumber = true
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayValue *= -1
+            } else if calcMethod == "AC" {
+                displayValue = 0
+            } else if calcMethod == "%" {
+                displayValue /= 100
+            }
+            
+        }
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text?.append(contentsOf: numValue)
+            }
+        }
+        
+    }
+    
+}
+
---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Salah Khaled on 6/4/20.
+//  Copyright © 2020 Salah Khaled. All rights reserved.
+//
+
+import UIKit
